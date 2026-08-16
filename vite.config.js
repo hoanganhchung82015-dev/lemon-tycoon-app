492b2ace2fc36367bb4f65edd8183ad1e7b1a999
hoanganhchung82015-dev <hoanganhchung82015@gmail.com>
2026-08-16 08:06:19 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lemon-tycoon-app/', // Thay lemon-tycoon-app bằng ĐÚNG tên repository trên GitHub
+  base: '/', // Với repository hoanganhchung82015-dev.github.io, base bắt buộc là '/'
 });
